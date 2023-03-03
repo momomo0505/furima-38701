6ed4da841c1978aea5add1b9e5c4f9103dec0c9d
momomo0505 <techcamp.fujita.t@gmail.com>
2023-03-03 21:30:33 +0900

    sessions/newのカラム名を修正
    バリデーション修正
    sessions/newとregistration/newのカラム名を修正

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,5 +4,14 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
-         validates :name, presence: true
+         validates :nickname, presence: true
+       
+
+         validates :email,    presence: true
+         validates :last_name, format: { with: /\A[ぁ-んァ-ン一-龥]/ }
+         validates :first_name, format: { with: /\A[ぁ-んァ-ン一-龥]/ }
+         validates :last_name_kana, format: { with: /\A[ァ-ヶー－]+\z/ }
+         validates :first_name_kana, format: { with: /\A[ァ-ヶー－]+\z/ }
+       
+
 end
