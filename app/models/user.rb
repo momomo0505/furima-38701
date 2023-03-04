a1e4496c994acac52927fb667372eef51e72414d
momomo0505 <techcamp.fujita.t@gmail.com>
2023-03-04 18:07:55 +0900

    13.14行目を修正
    「々」が入力された際に、バリデーションで弾かれてしまうため修正。
    ・長音記号(ー)も考慮（龥々ー）
    ・文字列の最後まで考慮（+\z）

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,12 +8,10 @@ class User < ApplicationRecord
 
   PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]+\z/i.freeze
   validates_format_of :password, with: PASSWORD_REGEX, message: 'Include both letters and numbers'
-
-  validates :email,    presence: true
   validates :last_name, format: { with: /\A[ぁ-んァ-ン一-龥]/ }
   validates :first_name, format: { with: /\A[ぁ-んァ-ン一-龥]/ }
-  validates :last_name_kana, format: { with: /\A[ァ-ヶー－]+\z/ }
-  validates :first_name_kana, format: { with: /\A[ァ-ヶー－]+\z/ }
+  validates :last_name_kana, format: { with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/ }
+  validates :first_name_kana, format: { with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/ }
 
   validates :nickname, presence: true
   validates :birthday, presence: true
