08ce342955a17a4de720dee993d9627965b6555b
momomo0505 <techcamp.fujita.t@gmail.com>
2023-03-05 20:05:42 +0900

    画像の保存を許可するストロングパラメーター

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -6,3 +6,9 @@ class ItemsController < ApplicationController
   def index
   end
 end
+
+private
+
+def item_params
+  params.require(:items).permit(:name, :description, :image, :category_id, :item_condition_id, :shipping_charge_id, :prefecture_id, :days_to_ship_id, :price ).merge(user_id: current_user.id)
+end
