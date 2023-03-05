24c5985418d594ece60e9ba237dfccbe8360b0d7
momomo0505 <techcamp.fujita.t@gmail.com>
2023-03-05 19:58:52 +0900

    バリデーション追加

--- app/models/item.rb
+++ app/models/item.rb
@@ -4,4 +4,16 @@ class Item < ApplicationRecord
   has_one :order
   has_one_attached :image
 
+  validates :user_id
+  validates :image
+  validates :name
+  validates :description
+  validates :category_id
+  validates :item_condition_id
+  validates :shipping_charge_id
+  validates :prefecture_id
+  validates :days_to_ship_id
+  validates :price
+
+
 end
