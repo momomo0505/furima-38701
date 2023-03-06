8c16482544a2a06c39698b40788be42da161af1c
momomo0505 <techcamp.fujita.t@gmail.com>
2023-03-06 22:11:41 +0900

    基本的に変更されないデータ実装5
    出品画面にて発送までの日数実装。

--- app/models/item.rb
+++ app/models/item.rb
@@ -5,6 +5,7 @@ class Item < ApplicationRecord
   belongs_to :item_condition
   belongs_to :shipping_charge
   belongs_to :prefecture
+  belongs_to :days_to_ship
 
 
   validates :name, :text, :image, presence: true
@@ -12,6 +13,7 @@ class Item < ApplicationRecord
   validates :item_condition_id, numericality: { other_than: 1 , message: "can't be blank"}
   validates :shipping_charge_id, numericality: { other_than: 1 , message: "can't be blank"}
   validates :prefecture_id, numericality: { other_than: 1 , message: "can't be blank"}
+  validates :days_to_ship_id, numericality: { other_than: 1 , message: "can't be blank"}
 
   belongs_to :user
   has_one :order
