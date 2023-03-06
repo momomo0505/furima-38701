7eaa7d3ab230bf63484c5694ed89c93a9886ff24
momomo0505 <techcamp.fujita.t@gmail.com>
2023-03-06 21:43:10 +0900

    基本的に変更されないデータ実装３
    出品画面にて配送料の負担の実装。

--- app/models/item.rb
+++ app/models/item.rb
@@ -3,12 +3,15 @@ class Item < ApplicationRecord
   extend ActiveHash::Associations::ActiveRecordExtensions
   belongs_to :category
   belongs_to :item_condition
+  belongs_to :shipping_charge
 
 
 
   validates :name, :text, :image, presence: true
   validates :category_id, numericality: { other_than: 1 , message: "can't be blank"}
   validates :item_condition_id, numericality: { other_than: 1 , message: "can't be blank"}
+  validates :shipping_charge_id, numericality: { other_than: 1 , message: "can't be blank"}
+  
 
   belongs_to :user
   has_one :order
