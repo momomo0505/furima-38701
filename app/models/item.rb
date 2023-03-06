17040ac8c9954a0d972c3fe3455c5fbff83892ab
momomo0505 <techcamp.fujita.t@gmail.com>
2023-03-06 21:51:15 +0900

    基本的に変更されないデータ実装２
    出品画面にて発送元の地域実装。

--- app/models/item.rb
+++ app/models/item.rb
@@ -4,14 +4,14 @@ class Item < ApplicationRecord
   belongs_to :category
   belongs_to :item_condition
   belongs_to :shipping_charge
-
+  belongs_to :prefecture
 
 
   validates :name, :text, :image, presence: true
   validates :category_id, numericality: { other_than: 1 , message: "can't be blank"}
   validates :item_condition_id, numericality: { other_than: 1 , message: "can't be blank"}
   validates :shipping_charge_id, numericality: { other_than: 1 , message: "can't be blank"}
-  
+  validates :prefecture_id, numericality: { other_than: 1 , message: "can't be blank"}
 
   belongs_to :user
   has_one :order
