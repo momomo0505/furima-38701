83fa4d0f89d5776521d22532d3a39c7c402ddd44
momomo0505 <techcamp.fujita.t@gmail.com>
2023-03-06 21:32:43 +0900

    基本的に変更されないデータ実装２
    出品画面にて商品の状態実装。

--- app/models/item.rb
+++ app/models/item.rb
@@ -2,10 +2,13 @@ class Item < ApplicationRecord
 
   extend ActiveHash::Associations::ActiveRecordExtensions
   belongs_to :category
+  belongs_to :item_condition
+
+
 
   validates :name, :text, :image, presence: true
   validates :category_id, numericality: { other_than: 1 , message: "can't be blank"}
-
+  validates :item_condition_id, numericality: { other_than: 1 , message: "can't be blank"}
 
   belongs_to :user
   has_one :order
