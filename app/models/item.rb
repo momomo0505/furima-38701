803200d01f77006a992b2c26e5bb35ecf4fa24d3
momomo0505 <techcamp.fujita.t@gmail.com>
2023-03-07 19:59:52 +0900

    販売価格 に範囲を設定
    バリデーションにて¥300〜9,999,999に。

--- app/models/item.rb
+++ app/models/item.rb
@@ -13,6 +13,7 @@ class Item < ApplicationRecord
   validates :shipping_charge_id, numericality: { other_than: 1 , message: "can't be blank"}
   validates :prefecture_id, numericality: { other_than: 1 , message: "can't be blank"}
   validates :days_to_ship_id, numericality: { other_than: 1 , message: "can't be blank"}
+  validates :price, numericality: { only_integer: true, greater_than_or_equal_to: 300, less_than_or_equal_to: 9999999}
   
 
   belongs_to :user
