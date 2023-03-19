f320ac0ced57ca409a12db7547dc5280430158c4
momomo0505 <techcamp.fujita.t@gmail.com>
2023-03-19 13:16:29 +0900

    編集画面実装１
    editとupdateのコントローラー設定。
    編集画面で詳細画面の情報が表示される。

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -34,22 +34,23 @@ end
   #end
 #end
 
-#def edit
-  #@item = Item.find(params[:id])
-  #if @item.user_id == current_user.id && @item.order.nil?
-  #else
-    #redirect_to root_path
-  #end
-#end
+def edit
+  @item = Item.find(params[:id])
+  if @item.user_id == current_user.id && @item.order.nil?
+  else
+    redirect_to root_path
+  end
+end
 
-#def update
-  #@item = Item.find(params[:id])
-  #if @item.valid?
-    #redirect_to item_path(item_params)
-  #else
-    #render :edit
-  #end
-#end
+def update
+  @item.update(item_params)
+  # バリデーションがOKであれば詳細画面へ
+  if @item.valid?
+    redirect_to item_path(item_params)
+  else
+    render :edit
+  end
+end
 
 private
 
