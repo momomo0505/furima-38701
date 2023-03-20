2be86ebe9745af715183628f1be969dcc3084635
momomo0505 <techcamp.fujita.t@gmail.com>
2023-03-20 22:28:15 +0900

    itemsコントローラーにdestroyメソッド追加

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -23,15 +23,15 @@ def create
   end
 end
 
-#def destroy
-  #@item = Item.find(params[:id])
-  #if @item.user_id == current_user.id
-    #@item.destroy
-    #redirect_to root_path
-  #else
-    #redirect_to root_path
-  #end
-#end
+def destroy
+  @item = Item.find(params[:id])
+  if @item.user_id == current_user.id
+    @item.destroy
+    redirect_to root_path
+  else
+    redirect_to root_path
+  end
+end
 
 def edit
   if @item.user_id == current_user.id && @item.order.nil?
