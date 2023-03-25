7348bb56372768f1d7c97ec7ad5cbc4652b68848
momomo0505 <techcamp.fujita.t@gmail.com>
2023-03-25 21:39:13 +0900

    購入画面表示まで
    購入機能に用いるモデルやコントローラーを作成およびアソシエーション。

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,11 @@
 Rails.application.routes.draw do
 
+  get 'orders/index'
+  get 'orders/create'
   devise_for :users
 
   root to: "items#index"
   resources :items do
-  # resources :orders, only: [:index, :create] 
+   resources :orders, only: [:index, :create] 
    end
 end
--- db/migrate/20230325105653_create_payments.rb
+++ db/migrate/20230325105653_create_payments.rb
@@ -1,7 +1,13 @@
 class CreatePayments < ActiveRecord::Migration[6.0]
   def change
-    create_table :payments do |t|
-
+    create_table   :payments do |t|
+      t.references :order,         null: false, foreign_key: true
+      t.string     :post_code,      null: false
+      t.integer    :prefecture_id, null: false
+      t.string     :city,          null: false
+      t.string     :block,         null: false
+      t.string     :building
+      t.string     :tel,  null: false
       t.timestamps
     end
   end
