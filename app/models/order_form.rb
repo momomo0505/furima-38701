ca057faae07fe46f45d53f9d8f3bb5db8c916651
momomo0505 <techcamp.fujita.t@gmail.com>
2023-03-25 09:05:18 +0900

    order_formモデルにクラス定義、バリデーション追加
    作成したクラスでActiveModel::Modelを呼び出して、form_withメソッドに対応する機能とバリデーションを行う機能を持たせる。
    attr_accessorを用いて、保存したい複数のテーブルのカラム名すべてを扱えるようにする。
    バリデーションの処理を書く。

--- app/models/order_form.rb
+++ app/models/order_form.rb
@@ -1,2 +1,15 @@
 class OrderForm < ApplicationRecord
+  class OrderForm
+    include ActiveModel::Model
+    attr_accessor :user_id,:item_id
+  
+    # ここにバリデーションの処理を書く
+    validates :user_id presence: true
+    validates :item_id presence: true
+  
+    def save
+      # 各テーブルにデータを保存する処理を書く
+      
+    end
+  end
 end
